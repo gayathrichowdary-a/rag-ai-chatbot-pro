bb4b23868e0ce3bc9e96bfcd5b96e9a738eb9924
A. Gayathri <your-github-email@example.com>
2026-05-28 17:24:27 +0530

    gayathrichowdary

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from collections import defaultdict
 import numpy as np
 
 from langchain_community.document_loaders import PyMuPDFLoader
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain.retrievers import EnsembleRetriever 
 from langchain_community.retrievers import BM25Retriever
